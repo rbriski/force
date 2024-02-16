9b9beda94065db99b9d9374e8f9007b7bd8a8d29
Bob Briski <bbriski@raybeam.com>
2024-02-16 11:42:45 -0800

    Adding test workers/workflows

---
+++ project/run_worker.py
@@ -0,0 +1,24 @@
+import asyncio
+
+# from temporalio import activity, workflow
+from temporalio.client import Client
+from temporalio.worker import Worker
+
+from activities import say_hello
+from workflows import SayHello
+
+
+async def main():
+    client = await Client.connect("temporal:7233", namespace="default")
+    # Run the worker
+    worker = Worker(
+        client,
+        task_queue="hello-task-queue",
+        workflows=[SayHello],
+        activities=[say_hello],
+    )
+    await worker.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ project/run_workflow.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from run_worker import SayHello
+from temporalio.client import Client
+
+
+async def main():
+    # Create client connected to server at the given address
+    client = await Client.connect("temporal:7233")
+    # Execute a workflow
+    result = await client.execute_workflow(
+        SayHello.run, "Temporal", id="hello-workflow", task_queue="hello-task-queue"
+    )
+
+    print(f"Result: {result}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
